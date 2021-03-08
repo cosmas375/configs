783a2b3952037377a97ed44dc1f054238d17591f
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2021-03-08 03:59:30 +0300

    vscode configs

---
+++ get_code_configs.sh
@@ -0,0 +1,3 @@
+mkdir -p code &&\
+cp ~/.config/Code/User/keybindings.json ./code/keybindings.json &&\
+cp ~/.config/Code/User/settings.json ./code/settings.json
---
+++ set_code_configs.sh
@@ -0,0 +1,2 @@
+cp ./code/keybindings.json ~/.config/Code/User/keybindings.json &&\
+cp ./code/settings.json ~/.config/Code/User/settings.json 
