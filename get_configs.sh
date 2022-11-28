4f89e4be723f84b82d6c04efbba4746da94acd3a
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2022-11-28 22:39:59 +0300

    code: vue snippets

--- get_configs.sh
+++ get_configs.sh
@@ -1,5 +1,6 @@
 mkdir -p code &&\
 cp ~/.config/Code/User/keybindings.json ./code/keybindings.json &&\
 cp ~/.config/Code/User/settings.json ./code/settings.json
+cp ~/.config/Code/User/snippets/vue.json ./code/snippets/vue.json
 
 cp ~/.vimrc ./.vimrc 
