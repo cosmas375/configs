c2ed58914ff9d295751990d464c37524352db158
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2023-02-11 23:30:03 +0100

    basic nvim config

---
+++ nvim/lua/cosmas375/init.lua
@@ -0,0 +1,4 @@
+require('cosmas375.explore')
+require('cosmas375.find')
+require('cosmas375.remap')
+require('cosmas375.set')
---
+++ nvim/lua/cosmas375/packer.lua
@@ -0,0 +1,5 @@
+return require('packer').startup(function(use)
+  use { "ellisonleao/gruvbox.nvim" }
+  use { "bling/vim-bufferline" }
+end)
+
