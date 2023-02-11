fd30111ec9c967e6fdb116816ada3d1d92238f17
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2023-02-12 00:00:48 +0100

    nvim

--- nvim/lua/cosmas375/find.lua
+++ nvim/lua/cosmas375/find.lua
@@ -5,14 +5,13 @@ vim.g.bufferline_show_bufnr = 0
 vim.opt.hidden = true
 
 vim.opt.wildignorecase = true
+vim.opt.wildignore = {
+    '*/node_modules/*',
+    '*/__pycache__/*',
+}
 vim.opt.wildoptions = "fuzzy,pum"
 
 vim.keymap.set("n", "<C-p>", [[:find ]])
 vim.keymap.set("n", "J", vim.cmd.bp)
 vim.keymap.set("n", "K", vim.cmd.bn)
 vim.keymap.set("n", "<C-w>", function() vim.cmd("bp") pcall(function() vim.cmd("bd #") end) end)
-
-vim.keymap.set("n", "<leader>h", function() vim.cmd("wincmd h") end)
-vim.keymap.set("n", "<leader>j", function() vim.cmd("wincmd j") end)
-vim.keymap.set("n", "<leader>k", function() vim.cmd("wincmd k") end)
-vim.keymap.set("n", "<leader>l", function() vim.cmd("wincmd l") end)
--- nvim/lua/cosmas375/remap.lua
+++ nvim/lua/cosmas375/remap.lua
@@ -7,3 +7,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<leader>h", function() vim.cmd("wincmd h") end)
+vim.keymap.set("n", "<leader>j", function() vim.cmd("wincmd j") end)
+vim.keymap.set("n", "<leader>k", function() vim.cmd("wincmd k") end)
+vim.keymap.set("n", "<leader>l", function() vim.cmd("wincmd l") end)
