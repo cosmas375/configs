7089c653d2c583099d6a2d5c866e3da2341d1455
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2024-12-23 00:28:47 +0100

    i3 apply script update

--- i3/_apply.sh
+++ i3/_apply.sh
@@ -1,2 +1,6 @@
-rm -rf ~/.config/i3* &&
-cp -r ./i3* ~/.config
+mkdir -p ~/.config/i3 &&
+cp ./i3/config ~/.config/i3/config &&
+
+mkdir -p ~/.config/i3status &&
+cp ./i3status/config ~/.config/i3status/config  &&
+cp ./i3status/i3script.sh ~/.config/i3status/i3script.sh
