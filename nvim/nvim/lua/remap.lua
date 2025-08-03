a7429a4c1a1fc117a10ec1b7e6a71245457c6134
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2025-08-03 12:44:52 +0300

    nvim wipe buffer on close

--- nvim/nvim/lua/remap.lua
+++ nvim/nvim/lua/remap.lua
@@ -16,7 +16,7 @@ vim.keymap.set("n", "<leader>l", function() vim.cmd("wincmd l") end)
 -- buffer navigation
 vim.keymap.set("n", "J", vim.cmd.bp)
 vim.keymap.set("n", "K", vim.cmd.bn)
-vim.keymap.set("n", "x", function() vim.cmd("bp") pcall(function() vim.cmd("bd #") end) end)
+vim.keymap.set("n", "x", function() vim.cmd("bp") pcall(function() vim.cmd("bw #") end) end)
 
 -- scroll
 vim.keymap.set("n", "<C-j>", "<C-e>")
