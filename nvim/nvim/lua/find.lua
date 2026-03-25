caf24faaef54676168207a57449606b44086ba74
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2026-03-25 23:40:37 +0300

    nvim: add grep function on <S-C-p>

--- nvim/nvim/lua/find.lua
+++ nvim/nvim/lua/find.lua
@@ -1,9 +1,7 @@
+-- find
 vim.cmd [[set path=$PWD/**]]
-
 vim.g.bufferline_show_bufnr = 0
-
 vim.opt.hidden = true
-
 vim.opt.wildignorecase = true
 vim.opt.wildignore = {
     '*/node_modules/*',
@@ -11,5 +9,24 @@ vim.opt.wildignore = {
     '*/__pycache__/*',
 }
 vim.opt.wildoptions = "fuzzy,pum"
-
 vim.keymap.set("n", "<C-p>", [[:find ]])
+
+-- grep
+vim.opt.grepprg = 'grep -rn'
+vim.api.nvim_create_user_command(
+  'Grep',
+  function(opts)
+    local pattern = opts.fargs[1]
+    local path = opts.fargs[2]
+
+    if path == nil then path = '' end
+
+    vim.cmd('grep ' .. pattern .. path)
+    vim.cmd('copen')
+  end,
+  {
+    nargs = '+',
+  }
+)
+vim.keymap.set("n", "<C-S-p>", [[:Grep ]])
+vim.keymap.set("n", "<A-C-S-p>", 'copen') -- results window
