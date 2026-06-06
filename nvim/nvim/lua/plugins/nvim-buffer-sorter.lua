086c53f9d13cd5098922b4c4514ca0de8ceb20a3
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2026-06-06 11:07:15 +0300

    nvim: add close all except current shortcut

--- nvim/nvim/lua/plugins/nvim-buffer-sorter.lua
+++ nvim/nvim/lua/plugins/nvim-buffer-sorter.lua
@@ -12,4 +12,22 @@ vim.keymap.set("n", "K", nvim_buffer_sorter.go_next)
 vim.keymap.set("n", "<<", nvim_buffer_sorter.shift_left)
 vim.keymap.set("n", ">>", nvim_buffer_sorter.shift_right)
 
+-- close
 vim.keymap.set("n", "x", function() vim.cmd("bp") pcall(function() vim.cmd("bw #") end) end)
+-- close all except the current
+vim.keymap.set(
+    "n",
+    "<S-C-x>",
+    function()
+        local buffers = nvim_buffer_sorter.get_buffers_list()
+        local current_buffer = vim.api.nvim_get_current_buf()
+
+        for i,v in pairs(buffers) do
+            if v == current_buffer then
+            else
+                vim.cmd("bp")
+                pcall(function() vim.cmd("bw "..v) end)
+            end
+        end
+    end
+)
